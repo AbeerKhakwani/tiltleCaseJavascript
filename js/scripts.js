aea64388338f288d9de29eb05129e40b201f2e56
Abeer Khakwani & Erica Cha <student@epicodus.com>
2015-04-07 11:33:26 -0700

    Add method to uncapitilize article

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,46 @@ var titleCaseTwo= function(stringToCaps){
      var wordString = returnedString.join(" ");
     return wordString;
 };
+
+// var titleCaseThree= function(stringToCaps){
+//
+//     var arrayOfWords=stringToCaps.split(" ");
+//     var returnedString=[];
+//     var articles = ["and", "if", "or", "else", "in", "to"];
+//     arrayOfWords.forEach(function(word) {
+//         if()
+//         var arrayLetters = word.split("");
+//         arrayLetters[0] = arrayLetters[0].toUpperCase();
+//         var wordCase = arrayLetters.join("");
+//          returnedString.push(wordCase);
+//
+//     });
+//
+//      var wordString = returnedString.join(" ");
+//     return wordString;
+//
+// };
+var article= function (word){
+
+    var articles = ["and", "if", "or", "else", "in", "to"];
+    var wordreturned= "";
+
+    if($.inArray(word, articles) > 0) {
+
+        var arrayOfWord = word.split("");
+        arrayOfWord[0]= arrayOfWord[0].toUpperCase();
+        var wordCase= arrayOfWord.join("");
+
+        wordreturned+= wordCase;
+        wordreturned+=word;
+
+
+    }
+    else {
+
+        wordreturned+=word;
+    }
+
+    return wordreturned;
+
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,12 @@
 describe('titleCase', function() {
     it("capitalizes the first letter of a word", function() {
-        expect(titleCase("cat")).to.equal("Cat");
+        expect(titleCaseTwo("cat")).to.equal("Cat");
     });
     it("capitalizes the first letter of two words", function() {
         expect(titleCaseTwo("hat cat")).to.equal("Hat Cat");
+    });
+    it("uncapitalizes the the articles", function() {
+        expect(article("and")).to.equal("and");
     });
 
 });
